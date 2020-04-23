cee10e7e72d0d7972533d73fc165da13a800c913
curious-metric-5246 <richardjharvey1@gmail.com>
2020-04-23 23:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function takeANumber (currentLine, customerName){
 
 function nowServing (currentLine){
   if(currentLine.length > 0){
-    console.log( "Currently serving " + currentLine[0] + ".");
     currentLine.shift();
+    return "Currently serving " + currentLine[0] + ".";
   } else{
     return "There is nobody waiting to be served!";
   }
