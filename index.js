c283f2c6f1dad20b71e8b0a982191cd42d4ab65f
curious-metric-5246 <richardjharvey1@gmail.com>
2020-04-23 23:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function takeANumber (currentLine, customerName){
+  console.log(currentLine.length)
   return "Welcome, " + customerName + ". You are number " + currentLine.length()+ " in line.";
 }
