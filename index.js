b631f693730fb25a01e14b85d2eb2f89ef719ba6
curious-metric-5246 <richardjharvey1@gmail.com>
2020-04-23 23:08:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber (currentLine, customerName){
   var currentPosition = currentLine;
-  return ("Welcome, " + ${customerName} + ". You are number " + currentPosition + " in line.");
+  return "Welcome, " + customerName + ". You are number " + currentPosition + " in line.";
 }
